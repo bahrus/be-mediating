722077371294de7b0f196d58928831ab355fca6a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-29 10:35:02 -0500

    Update README.md

---
+++ be-mediating.js
@@ -0,0 +1,25 @@
+// @ts-check
+import { BE } from 'be-enhanced/BE.js';
+import { propInfo, resolved, rejected } from 'be-enhanced/cc.js';
+import { dispatchEvent as de } from 'trans-render/positractions/dispatchEvent.js';
+
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP, AP, BAP, ObservingParameters} from './ts-refs/be-methodical/types.d.ts' */
+
+/**
+ * @implements {Actions}
+ */
+class BeMediating extends BE  {
+    de = de;
+    /**
+     * @type {BEConfig<BAP, Actions & IEnhancement, any>}
+     */
+    static config = {
+        propInfo: {
+            ...propInfo,
+        }
+    };
+}
+
+await BeMediating.bootUp();
+export {BeMediating};
